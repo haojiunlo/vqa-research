1f6fa44d153b1d922119a54adc9abf5c595d266c
haojiun.lo <haojiun.lo@shopback.com>
2023-05-19 01:19:58 +0800

    implement training process

--- src/train.py
+++ src/train.py
@@ -1,15 +1,35 @@
+import argparse
+
 import lightning.pytorch as pl
+import torch
 from torch.utils.data import DataLoader
 
+from src.models.lightning_module import LitVqaModel
 from src.models.ocr_embedding import OcrEmbedding
-from src.models.vqa_model import LitVqaModel
-from src.utils.datasets import TextVqaDataset
+from src.utils.datasets import CustomDataCollator, TextVqaDataset
+
+
+def setup_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--learning_rate", default=3e-5, type=float, help="learning rate"
+    )
+
+    return parser
+
 
 if __name__ == "__main__":
-    dataset = TextVqaDataset()
-    train_loader = DataLoader(dataset)
+    parser = setup_parser()
+    args = parser.parse_args()
+    dataset = TextVqaDataset(path="dataset")
+    train_loader = DataLoader(
+        dataset,
+        batch_size=2,
+        shuffle=True,
+        collate_fn=CustomDataCollator(dataset.decoder_tokenizer),
+    )
 
-    model = LitVqaModel(OcrEmbedding, None, None)
+    model = LitVqaModel(args)
 
     trainer = pl.Trainer()
     trainer.fit(model=model, train_dataloaders=train_loader)
